f4e5d2ce8ed521e23fd70079bb7e85ae9b99805c
Josh <freckletonj@gmail.com>
2017-05-21 22:56:09 -0600

    core-api-1: begin to flesh out some of the auth

--- src/Authenticate.hs
+++ src/Authenticate.hs
@@ -1,3 +1,26 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE Unsafe #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 
 module Authenticate where
+
+import Crypto.KDF.BCrypt (hashPassword, validatePassword)
+import Data.Aeson
+import Data.Aeson.TH
+import GHC.Generics (Generic)
+import Servant
+import Database.Persist
+
+
+data ClearCredentials = ClearCredentials { email :: String
+                                         , clearPass :: String}
+                 deriving (Generic, FromJSON)
+
+type AuthenticateAPI = "token" :> ReqBody '[JSON] ClearCredentials
+                       :> Post '[JSON] (String)
+                       
+                       :<|> "signup" :> ReqBody '[JSON] ClearCredentials
+                       :> Post '[JSON] NoContent
